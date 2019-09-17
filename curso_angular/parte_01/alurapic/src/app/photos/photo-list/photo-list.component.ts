245f5a63f433219baa69f7baa60cd4c6aa778564
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-09-17 16:32:49 -0300

    Conclusão parte 02 curso Angular

--- curso_angular/parte_01/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ curso_angular/parte_01/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -15,7 +13,6 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -29,14 +26,12 @@ export class PhotoListComponent implements OnInit {
 
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
   }
 
   load() {
     this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
     .subscribe(photos => {
+      this.filter = '';
       this.photos = this.photos.concat(photos);
       if(!photos.length) this.hasMore = false;
     });
