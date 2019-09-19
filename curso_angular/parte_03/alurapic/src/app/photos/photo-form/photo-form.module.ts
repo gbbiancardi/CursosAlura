32eed9d562b8e554d54e52e5fb581766aa509414
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-09-19 18:15:05 -0300

    Aula 01 Angular Parte 03

---
+++ curso_angular/parte_03/alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    declarations: [
+        PhotoFormComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule,
+        RouterModule
+    ]
+})
+
+export class PhotoFormModule {}
