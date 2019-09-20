71bbd2ee6c7d5aafad04a569c4521a824c31616f
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-09-20 17:34:04 -0300

    Aula 05 curso Angular Parte 03 finalizada

--- curso_angular/parte_03/alurapic/src/app/photos/photo/photo.service.ts
+++ curso_angular/parte_03/alurapic/src/app/photos/photo/photo.service.ts
@@ -49,4 +49,9 @@ export class PhotoService {
             { commentText }
         );
     }
+
+    removePhoto(photoId: number) {
+
+        return this.http.delete(API + '/photos/' + photoId);
+    }
 }
