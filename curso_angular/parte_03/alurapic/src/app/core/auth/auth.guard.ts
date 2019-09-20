812a0b19c63f4c019d0249b7195dc6e2540a233f
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-09-20 15:13:58 -0300

    Aula 02 Angular Parte 03 Finalizada

--- curso_angular/parte_03/alurapic/src/app/core/auth/auth.guard.ts
+++ curso_angular/parte_03/alurapic/src/app/core/auth/auth.guard.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
@@ -7,20 +8,19 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 
-export class AuthGuard implements CanActivate{
-    
+export class AuthGuard implements CanActivate { 
     constructor(
         private userService: UserService,
         private router: Router
-    ) {}
+    ) { }
 
     canActivate(
         route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
             
-            if(this.userService.isLogged()){
+            if(!this.userService.isLogged()){
 
-                this.router.navigate(['user', this.userService.getUserName()]);
+                this.router.navigate(['']);
                 return false;
             }
             
