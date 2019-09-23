ddafcb67518a091db3c55f85da3238aa2169207c
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-09-23 15:33:31 -0300

    Aula 01 curso Angular 04 finalizada

---
+++ curso_angular/parte_04/alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, Renderer, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/user/user.service';
+
+@Directive({
+    selector: '[showIfLogged]'
+})
+
+export class showIfLoggedDirective implements OnInit {
+
+    constructor(
+        private element: ElementRef<any>,
+        private renderer: Renderer,
+        private userService: UserService
+    ) { }
+
+    ngOnInit(): void {
+        
+        !this.userService.isLogged()
+            && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+    }
+}
