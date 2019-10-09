9b67adfb4cae5de4e8740054abd0dd21e1095ead
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-10-09 14:39:50 -0300

    Spring I Aula 08

--- curso_spring/ws-spring/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ curso_spring/ws-spring/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -20,7 +20,7 @@ import br.com.casadocodigo.loja.models.TipoPreco;
 import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
-@RequestMapping("produtos")
+@RequestMapping("/produtos")
 public class ProdutosController {
 
 	@Autowired
@@ -32,7 +32,7 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping("/form")
-	public ModelAndView form() {
+	public ModelAndView form(Produto produto) {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		
@@ -44,14 +44,14 @@ public class ProdutosController {
 			RedirectAttributes redirectAttributes) {
 		
 		if (result.hasErrors()) {
-			return form();
+			return form(produto);
 		}
 		
 		produtoDao.gravar(produto);
 		
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
 		
-		return new ModelAndView("redirect:produtos");
+		return new ModelAndView("redirect:/produtos");
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
