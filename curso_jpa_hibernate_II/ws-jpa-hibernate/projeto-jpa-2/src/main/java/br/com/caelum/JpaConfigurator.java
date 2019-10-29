5b36390b5b76a82c556ea1588122784389044f7e
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-10-29 11:14:07 -0300

    Gerenciando conexões com Pool de conexão

--- curso_jpa_hibernate_II/ws-jpa-hibernate/projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
+++ curso_jpa_hibernate_II/ws-jpa-hibernate/projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
@@ -1,5 +1,6 @@
 package br.com.caelum;
 
+import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,25 +8,30 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class JpaConfigurator {
 
 	@Bean
-	public DataSource getDataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	public DataSource getDataSource() throws PropertyVetoException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
-	    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	    dataSource.setUsername("root");
+	    dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+	    dataSource.setUser("root");
 		dataSource.setPassword("P@lmeiras123");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/projeto_jpa?useTimezone=true&serverTimezone=UTC");
+		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/projeto_jpa?useTimezone=true&serverTimezone=UTC");
 
+		dataSource.setMinPoolSize(3);
+		dataSource.setMaxPoolSize(5);
+		dataSource.setNumHelperThreads(15);
+		
 	    return dataSource;
 	}
 
