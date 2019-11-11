99685407a23e9bb0b28ece37190f7287e8fffe45
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-11-11 14:54:16 -0300

    Aula 03 Proteção com Spring Security

--- curso_spring_boot/ws-spring-boot-2/forum/src/main/resources/data.sql
+++ curso_spring_boot/ws-spring-boot-2/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$u7pu2N6VL/amkOcuiqdYPeL9CYOoZCRLyF5Ejor9tB1/honMvTyVa');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
