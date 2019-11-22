293719e95e1afd42b4431ea7a4ca75556d62e48d
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-11-22 16:23:12 -0300

    Criando o gráfico de colunas - não finalizado

--- curso_graficos_web_I/bytebank/graficos.js
+++ curso_graficos_web_I/bytebank/graficos.js
@@ -57,17 +57,54 @@ function desenharGraficos() {
         title: 'Gastos por mês',
         width: 650,
         height: 300,
-        vAxis: { 
-            format: 'currency', 
-            gridlines: { 
-                count: 5, 
-                color: 'transparent' 
-            } 
+        vAxis: {
+            format: 'currency',
+            gridlines: {
+                count: 5,
+                color: 'transparent'
+            }
         },
         curveType: 'function',
         legend: 'none'
     }
 
     var grafico = new google.visualization.LineChart(document.getElementById('graficoLinha'));
+    grafico.draw(tabela, opcoes);
+
+    // Gráfico de Colunas
+    var tabela = google.visualization.arrayToDataTable(
+        [
+            ['Mês', 'Entrada', 'Saída'],
+            ['jan', 2500, 1000],
+            ['fev', 1000, 500],
+            ['mar', 3000, 1300],
+            ['abr', 1500, 1700],
+            ['mai', 5000, 2250],
+            ['jun', 3567, 3000],
+            ['jul', 3452, 1468],
+            ['ago', 1833, 5250],
+            ['set', 1800, 1000],
+            ['out', 1800, 1000],
+            ['nov', 3569, 1500],
+            ['dez', 3000, 1740]
+        ]);
+
+    var opcoes = {
+
+        title: 'Entradas e saídas da conta',
+        width: 800,
+        height: 400,
+        vAxis: {
+            gridlines:{color:'transparent'},
+            format: 'currency',
+            title: 'Valores'
+        },
+        hAxis: {
+            title: 'Mês'
+        }
+
+    }
+
+    var grafico = new google.visualization.ColumnChart(document.getElementById('graficoColuna'));
     grafico.draw(tabela, opcoes);
 }
