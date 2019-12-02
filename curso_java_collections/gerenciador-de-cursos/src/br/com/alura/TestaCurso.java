4ba31fd8f508096b5c57bd60a40b915a44fb9456
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2019-12-02 11:20:28 -0300

    First commit Java: Dominando Collections

---
+++ curso_java_collections/gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,31 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+
+		// Não é mais possível adicionar da forma abaixo pois agora a classe está defendendo a Lista
+		// javaColecoes.getAulas().add(new Aula("Revistando as ArrayLists", 21));
+		
+		javaColecoes.adiciona(new Aula("Trabalhando as ArrayLists", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando Coleções", 22));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
+		
+		List<Aula> aulas = new ArrayList<Aula>(aulasImutaveis);
+		
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		System.out.println(javaColecoes.getTempoTotal());
+		
+		System.out.println(javaColecoes);
+
+	}
+}
